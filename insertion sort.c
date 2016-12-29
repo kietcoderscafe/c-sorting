092446aea4429e19ac3f161b77e5e055b9d84bf8
RMouni <rimmalapudimounika@gmail.com>
2016-12-29 14:15:51 +0530

    added a c file

--- insertion sort.c
+++ insertion sort.c
@@ -5,25 +5,24 @@
 /*    Sorting Elements of an array in ascending order 
                               Using INSERTION SORT algorithm    */
 
-/* including header file stdio.h for input and output */
+// including header file stdio.h for input and output
 #include<stdio.h>
-int main()   /*it is the main function where the execution begins */
+int main()   //  it is the main function where the execution begins
 {
-// declaring variables that are used in the program
+// declaring variables i,j,s,temp and a[20]
    int i,j,s,temp,a[20];
-   printf("****** INSERTION SORT *****"); // displays insertion sort in the output screen 
+   printf("****** INSERTION SORT *****"); // displays as "INSERTION SORT" in the output screen 
    printf("\n Enter total elements: ");
-// reads input from the user
-   scanf("%d",&s);
+   scanf("%d",&s);  // reads input from the user
    printf("Enter %d elements: ",s);
    for(i=0;i<s;i++) // requesting  input from the user and iterating to obtain an array
    {
 	 scanf("%d",&a[i]);
    }
-// we use for loop which starts from i with value 1 and assign the value of i index in the variable temp
+// we use 'for loop' which starts from i with value 1 and assign the value of i index to the variable temp
    for(i=1;i<s;i++)
    {
-// temp is assigned with every element of array a[] in iteration
+// temp is assigned with every value of array a[i] in iteration
       temp=a[i];
 // try from the (i-1)th position
       j=i-1;
@@ -31,16 +30,16 @@ int main()   /*it is the main function where the execution begins */
                          to one position ahead of their current position */
       while((temp<a[j])&&(j>=0))
 	  {
-        a[j+1]=a[j];   // move the temp to the next position
-        j=j-1;        //  decrementing the positin of j 
+        a[j+1]=a[j];   // move temp to the next position
+        j=j-1;        // decrementing the positin of j 
       }
-      a[j+1]=temp;    //  temp is inserted in the appropriate position as needed to be attained 
+      a[j+1]=temp;    // temp is inserted in the appropriate position as needed to be attained 
    }
-// diplaying title of the result
+// diplay title of the result
    printf("After sorting: ");
    for(i=0;i<s;i++)   // iterating the output as entire elements of the array need to be displayed 
    {
-      printf(" %d",a[i]); //  display the result to the user
+      printf(" %d",a[i]); //  showing the sorted result
    }
 // as we are using int function we need to return 0 to show successful exit
    return 0; 
