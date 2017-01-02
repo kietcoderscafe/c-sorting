118bcd7d17eecdcaf1d8686cb0b870598e3110a9
Vineethavelaga <vineethavelaga1999@gmail.com>
2017-01-02 15:55:50 +0530

    Update selection_sort.c

--- selection_sort.c
+++ selection_sort.c
@@ -13,46 +13,46 @@
  void main()
  {
  /* declaring variables */
- int a[10], i, n;
- printf("\n enter the number of elements in the array: ");
- scanf("%d", &n);
- printf("\n enter the elements of the array: ");
+    int a[10], i, n;
+    printf("\n enter the number of elements in the array: ");
+    scanf("%d", &n);
+    printf("\n enter the elements of the array: ");
  /* the loop starts iterating and the elements will be entered */
- for(i=0;i<n;i++)
- {
- scanf("%d", &a[i]);
- }
+    for(i=0;i<n;i++)
+    {
+       scanf("%d", &a[i]);
+    }
  selection_sort(a, n);
  printf("\n the sorted array is: \n");
  /* the loop starts iterating and the elements get sorted in ascending order */
- for(i=0;i<n;i++)
- {
- printf(" %d\t", a[i]);
- }
- }
+    for(i=0;i<n;i++)
+    {
+       printf(" %d\t", a[i]);
+    }
  int smallest(int a[], int k, int n)
  {
- int pos = k, small=a[k], i;
- for(i=k+1;i<n;i++)
- {
+      int pos = k, small=a[k], i;
+      for(i=k+1;i<n;i++)
+      {
  /* the statement gets executed if arr[i] is less than smallest element */
  if(a[i]< small)
- {
- small = a[i];
- pos = i;
- }
- }
+  {
+   small = a[i];
+   pos = i;
+  }
+       }
  /* it returns the sorted element which will be stored in temperory array */
- return pos;
- }
+  return pos;
+  }
  void selection_sort(int a[],int n)
  {
- int k, pos, temp;
- for(k=0;k<n;k++)
- {
- pos = smallest(a, k, n);
- temp = a[k];
- a[k] = a[pos];
- a[pos] = temp;
+     int k, pos, temp;
+     for(k=0;k<n;k++)
+       {
+         pos = smallest(a, k, n);
+         temp = a[k];
+         a[k] = a[pos];
+         a[pos] = temp;
+        }
  }
  }
